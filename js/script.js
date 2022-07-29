5697d0e4454d4804102d8bc2a93660990f90e095
Gabriel Ghiaroni <gabrielghiaroni@outlook.com>
2022-07-29 16:34:23 -0300

    implementacao javascript faq

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@ animaisImagem.forEach((imagem, index) => {
         ativarDescricao(index);
     });
 });
+
+const perguntas = document.querySelectorAll('.faq-lista dt');
+const descricaoPerguntas = document.querySelectorAll('.faq-lista dd');
+
+descricaoPerguntas[0].classList.toggle('faq-lista-ativo');
+
+function mostrarDescricao(event) {
+    const perguntaClicada = event.target;
+    perguntaClicada.classList.toggle('faq-lista-ativo');
+    perguntaClicada.nextElementSibling.classList.toggle('faq-lista-ativo');
+};
+
+perguntas.forEach(pergunta => pergunta.addEventListener('click', mostrarDescricao));
