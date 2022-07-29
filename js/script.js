3a4625bfb5ba5b7f8299e3dff486d0a17df349f5
Gabriel Ghiaroni <gabrielghiaroni@outlook.com>
2022-07-29 00:16:16 -0300

    navegacao fotos-descricao

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('olá turma!')
+//
+//
+//
+//
+//
+const animaisImagem = document.querySelectorAll('.animais-lista li img');
+const animaisDescricao = document.querySelectorAll('.animais-descricao section');
+
+animaisDescricao[0].classList.add('ativo');
+
+function ativarDescricao(index) {
+
+    animaisDescricao.forEach(descricao => descricao.classList.remove('ativo'));
+    animaisDescricao[index].classList.add('ativo');
+};
+
+animaisImagem.forEach((imagem, index) => {
+    imagem.addEventListener('click', () => {
+        ativarDescricao(index);
+    });
+});
